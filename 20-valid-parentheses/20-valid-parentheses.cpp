1b519ee315633211e221459f29c7da9511f48d72
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-03-31 23:02:19 +0900

    Time: 4 ms (23.04%), Space: 6.5 MB (9.77%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
+    unordered_map<char, char> map;
+    stack<char> st;
+    void sett() {
+        map['('] = map['['] = map['{'] = 0, map[')'] = '(', map[']'] = '[', map['}']= '{';
+    }
+    
     bool isValid(string s) {
-        stack<char> st;
-        unordered_map<char, char> map;
-        map['('] = map['['] = map['{'] = 0;
-        map[')'] = '(', map[']'] = '[', map['}']= '{';
-
+        sett();
         for(auto& c : s) 
             if(map[c]) {
                 if(st.empty() || st.top() != map[c]) return false;
