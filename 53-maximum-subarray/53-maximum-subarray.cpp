eb325047f2790fb7b5757bee8390b7bb945899be
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-04 16:53:30 +0900

    Time: 197 ms (15.40%), Space: 67.7 MB (91.30%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int _max = -10000;
-        int sum = 0;
-        for(auto& i : nums) {
-            sum = sum < 0 ? i : sum + i;
-            if(_max < sum) _max = sum;
+    int divconq(vector<int>& nums, int left, int right) {
+        if(left==right) return nums[left];
+        int mid = left + right >> 1;
+        int _max = max(divconq(nums, mid + 1, right), divconq(nums, left, mid));
+        int left_max = -10000, right_max = -10000;
+
+        for(int i = mid, left_sum = 0; left <= i; i--) {
+            left_sum += nums[i];
+            if(left_max < left_sum) left_max = left_sum;
         }
-        return _max;
+        for(int i = mid + 1, right_sum = 0; i <= right; i++) {
+            right_sum += nums[i];
+            if(right_max < right_sum) right_max = right_sum;
+        }
+        return max(_max, left_max + right_max);
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        return divconq(nums, 0, nums.size() - 1);
     }
 };
