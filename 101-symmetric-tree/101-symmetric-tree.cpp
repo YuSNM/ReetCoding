d079766dbc46fc82f298a3c4edf0b708943bd232
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-06 19:08:09 +0900

    Time: 12 ms (23.86%), Space: 16.4 MB (42.14%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -10,31 +10,15 @@
  * };
  */
 class Solution {
-public:
+    public:
+    bool DFS(TreeNode* A, TreeNode* B) {
+        if(A == nullptr && B == nullptr) return true;
+        else if(A == nullptr ^ B == nullptr) return false;
+        else if(A->val != B->val) return false;
+        return DFS(A->left, B->right) && DFS(A->right, B->left);
+    }
+
     bool isSymmetric(TreeNode* root) {
-        queue<TreeNode *> q1, q2;
-        q1.push(root->left);
-        q2.push(root->right);
-
-        while (1) {
-            if(q1.empty() || q2.empty()) break;
-            TreeNode* node1 = q1.front();
-            TreeNode* node2 = q2.front();
-            q1.pop();    
-            q2.pop();
-            if(node1 == nullptr && node2 == nullptr) continue;
-            else if(node1 == nullptr ^ node2 == nullptr) return false;
-            else if(node1->val != node2->val) return false;
-
-            //왼쪽시작 노드는 왼쪽부터
-            q1.push(node1->left);
-            q1.push(node1->right);
-
-            //오른쪽 시작 노드는 오른쪽부터 큐삽입
-            q2.push(node2->right);
-            q2.push(node2->left);
-
-        }
-        return true;
+        return DFS(root->left, root->right);
     }
 };
