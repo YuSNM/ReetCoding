649c9ddbca51465337b318a29d32ec7e49e1ed29
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-06 20:12:24 +0900

    Time: 429 ms (45.58%), Space: 10.4 MB (74.83%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,9 +1,8 @@
-#define MOD 1000000007
 class Solution {
     int HashT[301];
+    const int mod = 1000000007;
 public:
     int threeSumMulti(vector<int>& arr, int target) {
-        const int mod = 1000000007;
         int ans = 0;
         for(int i = 1; i < arr.size(); ++i) {
             if(target < arr[i]) continue;
