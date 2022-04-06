30d11e33650c591b43b3f47fd0d1ff5e55d0a03a
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-06 19:16:08 +0900

    Time: 3 ms (89.86%), Space: 16.4 MB (73.72%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -11,12 +11,13 @@
  */
 class Solution {
     public:
-    bool isSymmetric(TreeNode* root) {
-        return DFS(root->left, root->right);
-    }
     bool DFS(TreeNode* A, TreeNode* B) {
         if(A == nullptr && B == nullptr) return true;
-        else if(A == nullptr || B == nullptr || A->val != B->val) return false;
-        return DFS(A->left, B->right) && DFS(A->right, B->left);
+        else if(A == nullptr ^ B == nullptr || A->val != B->val) return false;
+        return DFS(A->left, B->right) & DFS(A->right, B->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        return DFS(root->left, root->right);
     }
 };
