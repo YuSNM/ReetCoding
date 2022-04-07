6fd741a421484062c9da5293c2b606205844c242
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-07 11:12:55 +0900

    Time: 11 ms (94.85%), Space: 10.4 MB (74.57%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,14 +1,30 @@
+
 class Solution {
     const int mod = 1000000007;
+    int HashT[301] = {0, };
 public:
-    int threeSumMulti(vector<int>& arr, int target) {
-        int ans = 0;
-        unordered_map<int, int> um;
-        for(int i = 1; i < arr.size(); ++i) {
-            ans = (ans + um[target - arr[i]]) % mod;
-            for(int j = 0; j < i; ++j) 
-                ++um[arr[i] + arr[j]];
+    int Combi(long long n, int r) {
+        for(int i = n - 1, f = 2; f <= r; i--, f++) 
+            n = n * i / f;
+        return n % mod;
+    } 
+
+    int func(vector<int>& arr, int target, int d, int start) {
+        if (d == 1) return target >= start ? HashT[target] : 0;
+        else if (d == 0) return target == 0;
+        long long ans = 0;
+        int i, i_max;
+        for (i = start, i_max = target / d; i <= i_max; i++) {
+            if (!HashT[i]) continue;
+            for (int j = 1; j <= d; j++) 
+                if(HashT[i] >= j) 
+                    ans = (ans + func(arr, target - i * j, d - j, i + 1) * Combi(HashT[i], j))% mod;
         }
         return ans;
+    }
+
+    int threeSumMulti(vector<int>& arr, int target) {
+        for(auto& i : arr) ++HashT[i];
+        return func(arr, target, 3, 0);
     }
 };
