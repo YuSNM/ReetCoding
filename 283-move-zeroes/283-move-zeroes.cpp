18ee49fc71a15716bf1e6ff85835d94f41f3f7c3
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-07 17:09:47 +0900

    Time: 27 ms (70.61%), Space: 19.7 MB (7.03%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,8 +1,5 @@
 class Solution {
 public:
-    static bool cmp(const int a, const int b) {
-        return b==0;
-    }
     void moveZeroes(vector<int>& nums) {
         vector<int> v(nums.size(), 0);
         int j = 0;
