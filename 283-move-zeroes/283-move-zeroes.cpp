66d2b16ec5d317e95b7363c68fe5bb33e98d9588
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-07 17:27:30 +0900

    Time: 26 ms (73.14%), Space: 19.3 MB (13.94%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i, j;
-        for(i = 0, j = 0; i < nums.size(); ++i) 
-            if(nums[i]) nums[j++] = nums[i]; 
-        fill(nums.begin()+j, nums.end(), 0);
+        stable_partition(nums.begin(), nums.end(), [](int n) {return n;});
     }
 };
