ea57ee46eebb964f997da7f3a13a7411543c7a1d
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-07 12:58:42 +0900

    Update and rename 977-squares-of-a-sorted-array.cpp to Case2.cpp

--- 977-squares-of-a-sorted-array/Case2.cpp
+++ 977-squares-of-a-sorted-array/Case2.cpp
@@ -1,14 +1,15 @@
+//Time: 41 ms (63.29%), Space: 26 MB (54.88%) - LeetHub
+
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         vector<int> v(nums.size());
         
-        for(int p = nums.size(), left = 0, right = nums.size() - 1; p > 0;) {
+        for(int p = nums.size(), left = 0, right = nums.size() - 1; p > 0;) 
             if(abs(nums[left]) >= abs(nums[right]))
                 v[--p] = nums[left] * nums[left++];
             else 
                 v[--p] = nums[right] * nums[right--];
-        }
         
         
         return v;
