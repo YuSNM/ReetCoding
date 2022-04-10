e970f353ab39ea5258d92ea375280bc341967423
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-10 12:20:33 +0900

    Update and rename 136-single-number.cpp to Case1.cpp

--- 136-single-number/Case1.cpp
+++ 136-single-number/Case1.cpp
@@ -1,9 +1,11 @@
+// Time: 19 ms (81.91%), Space: 17.2 MB (17.51%) - LeetHub
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
+        
         int ans = 0;
         for(auto& i : nums) ans ^= i;
         return ans;
