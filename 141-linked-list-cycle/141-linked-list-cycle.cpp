60f4e274f4cdee8ca58aa2bde7db4edeb7b9b4bf
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-10 22:22:50 +0900

    Time: 24 ms (14.83%), Space: 7.9 MB (80.64%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,9 +9,11 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        for(ListNode *node = head; node; node = node->next) {
-            if(node->val == INT_MAX) return true;
-            node->val = INT_MAX;
+        ListNode *p1 = head, *p2 = head;
+        while (p1 && p1->next) {
+            p1 = p1->next->next;
+            p2 = p2->next;
+            if (p1==p2) return true;
         }
         return false;
     }
