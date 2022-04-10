a3701ed5976e8d7acefe91cd89f1305283a0f401
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-10 14:05:47 +0900

    Time: 4 ms (71.34%), Space: 8.3 MB (95.60%) - LeetHub

--- 682-baseball-game/682-baseball-game.cpp
+++ 682-baseball-game/682-baseball-game.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int calPoints(vector<string>& ops) {
-        stack<int> st;
-        int ans = 0;
+        vector<int> st(ops.size());
+        int j = 0;
 
-        for (auto& i : ops) 
-            if(i=="C") st.pop();
-            else if (i=="D") st.push(st.top() * 2);
-            else if (i=="+") {
-                int temp1 = st.top();
-                st.pop();
-                int temp2 = st.top();
-                st.push(temp1);
-                st.push(temp1 + temp2); 
+        for (auto& i : ops) {
+            if(i=="C") {
+                --j;
+                continue;    
             }
-            else st.push(stoi(i));
-
-        for(;!st.empty();st.pop()) ans += st.top();
-        return ans;
+            if (i=="D") 
+                st[j++] = st[j - 1] * 2;
+            else if (i=="+") 
+                st[j++] = st[j - 1] + st[j - 2];
+            else 
+                st[j++] = stoi(i);
+        }
+        
+        return accumulate(st.begin(), st.begin() + j, 0);
     }   
 };
