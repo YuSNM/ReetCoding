674f75d51ec65413037a19cb5eac7717f852ddba
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-11 00:04:13 +0900

    Time: 11 ms (80.93%), Space: 7.6 MB (81.62%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         vector<int> alpcnt(127, 0);
-        
-        for (int i = 0, bef = 0, _max = 0;;++alpcnt[s[i++]]) 
-            if (i == s.size()) {
-                _max = max(i - bef, _max);  //max 갱신
-                return _max;
-            }
-            else if (alpcnt[s[i]]) {
-                _max = max(i - bef, _max);  //max 갱신
-                do --alpcnt[s[bef++]];
-                while (alpcnt[s[i]]);
-            }
-        
+        int _max = 0, i = 0, bef = 0;
+        for (; i < s.size(); ++alpcnt[s[i++]]) {
+            _max = max(i - bef, _max);
+            while (alpcnt[s[i]]) 
+                --alpcnt[s[bef++]];
+        }
+        return max(i - bef, _max);
     }
 };
