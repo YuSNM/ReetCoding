f5d902551b86a8a5f584b7b098e8b991b5e6ea16
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-11 13:29:56 +0900

    Time: 5 ms (87.02%), Space: 7.3 MB (42.48%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int CountS1[128];
+    int CountS2[128];
+public:
+    bool checkCnt(int *a, int *b) {
+        for(int c = 'a'; c <= 'z'; c++)
+            if(a[c] != b[c]) return false;
+        return true;
+    }
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size() > s2.size()) return false;
+        
+        for(auto& i : s1) ++CountS1[i];
+        
+        int i = 0;
+        while(i < s1.size()) ++CountS2[s2[i++]];
+        if (checkCnt(CountS1, CountS2)) return true;
+        
+        while(i < s2.size()) {
+            --CountS2[s2[i - s1.size()]];   //1 CountS2 갱신
+            ++CountS2[s2[i++]]; 
+        
+            if (checkCnt(CountS1, CountS2)) return true;    //2. count 체크
+        }
+        
+        return false;
+    }
+};
