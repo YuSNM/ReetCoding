9ca65f8a242f731a9e0b1e38c4d7e93483ff02f7
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-11 18:25:25 +0900

    Time: 15 ms (42.74%), Space: 14 MB (80.69%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    const int dx[4] = {0, -1, 0, 1}, dy[4] = {-1, 0, 1, 0};
+    int NewC, OldC;
+public:
+    void dfs(vector<vector<int>>& image, int sr, int sc) {
+        image[sr][sc] = NewC;
+        for(int i = 0; i < 4; i++) {
+            int x = dx[i] + sr;
+            int y = dy[i] + sc;
+            if(x < 0 || x >= image.size() || y < 0 || y >= image[0].size() || image[x][y] != OldC) continue;
+            dfs(image, x, y);
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) return image;
+        NewC = newColor;
+        OldC = image[sr][sc];
+        dfs(image, sr, sc);
+        return image;
+    }
+};
