0374a0381deda7f863fc69f8e6429f06117c8f88
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-13 14:50:34 +0900

    Time: 22 ms (68.88%), Space: 19.6 MB (80.05%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -3,12 +3,9 @@ public:
     int majorityElement(vector<int>& nums) {
         int major = nums[0], cnt = 0;
         for (auto& i : nums) {
-            if(major != i) {
-                if(--cnt < 0)
-                    major = i, cnt = 1;
-                continue;
-            }  
-            ++cnt;
+            if(!cnt) 
+                major = i;
+            cnt += major != i ? -1 : 1;
         }
         return major;
     }
