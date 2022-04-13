13ed9186099f4d60ecbdba99f2fd46a7531f642c
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-13 16:24:22 +0900

    Time: 19 ms (85.09%), Space: 16.6 MB (96.67%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    void dfs(Node* node) {
+        if(!node->left) return;
+        
+        node->left->next = node->right;        
+        if(node->next)
+            node->right->next = node->next->left;
+        
+        dfs(node->right);
+        dfs(node->left);
+    }
+    Node* connect(Node* root) {
+        if(root)
+            dfs(root);
+        return root;
+    }
+};
