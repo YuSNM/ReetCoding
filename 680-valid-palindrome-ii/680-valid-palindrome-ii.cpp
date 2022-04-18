ed63e272891900489a260d627d5020f1cf02d061
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-18 15:42:35 +0900

    Time: 47 ms (98.53%), Space: 23 MB (53.25%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    bool check;
+public:
+    bool validPalindrome(string s) {
+        bool ans = 0;
+        int i = -1;
+        int j = s.size();
+        
+        while (++i < --j)            
+            if(s[i] != s[j]) 
+                if(!check) {
+                    check = 1;
+                    return validPalindrome(s.substr(i, j - i)) || validPalindrome(s.substr(i + 1, j - i));
+                }else
+                    return 0;
+        
+        return 1;
+    }
+};
