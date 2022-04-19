04ddf45e78b12e1036603c14b74c78a86b797ad8
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-20 02:44:13 +0900

    Time: 7 ms (98.69%), Space: 9 MB (79.01%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void backt(vector<vector<int>>& vvi, vector<int>& vi, int n, int k) {
+        if (k < 0) {
+            vvi.push_back(vi);
+            return;
+        }
+        for (int i = n; i > 0; i--) {
+            vi[k] = i;
+            backt(vvi, vi, i - 1, k - 1);
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> vvi;
+        vector<int> vi(k);
+        backt(vvi, vi, n, k - 1);
+        return vvi;
+    }
+};
