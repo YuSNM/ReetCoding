c8168111dd47cfee89d3216139ed199176aa8bb4
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-20 15:08:43 +0900

    Time: 9 ms (44.99%), Space: 8.5 MB (90.82%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int _min = INT_MAX;
+        int _min = 10000;
         
-        for (int i = 0, j, i_max = triangle.size() - 1; i < i_max; i++) {
+        for (int i = 0, i_max = triangle.size() - 1, j, j_max; i < i_max; i++) {
             triangle[i + 1][0] += triangle[i][0];
             
-            for (j = 1; j < triangle[i].size(); j++)
+            for (j = 1, j_max = triangle[i].size(); j < j_max; j++)
                 triangle[i + 1][j] += min(triangle[i][j - 1], triangle[i][j]);
             
             triangle[i + 1][j] += triangle[i][j - 1];
