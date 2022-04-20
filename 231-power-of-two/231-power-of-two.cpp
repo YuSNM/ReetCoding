94da369baecf2d4ee12bb271536cec6aeff7f493
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-20 15:47:40 +0900

    Time: 4 ms (37.95%), Space: 6 MB (25.99%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        
         if (n <= 0) return false;
-        for (; n > 1; n >>= 1) 
-            if (n & 1) 
-                return false;
-        return true;
+        return !(n & (n-1));
     }
 };
