ff4759094b3b0c64b58f442fed5f7f08bf48c001
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-20 11:08:29 +0900

    Time: 23 ms (67.66%), Space: 17 MB (51.81%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        
         int ans = 0;
         for(auto& i : nums) ans ^= i;
         return ans;
