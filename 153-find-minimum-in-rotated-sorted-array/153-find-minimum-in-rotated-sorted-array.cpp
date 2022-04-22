2c9ba6b3dee735f9e91145aadf01b6c6a3120df9
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-04-22 19:55:22 +0900

    Time: 0 ms (100.00%), Space: 10.2 MB (23.30%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -6,13 +6,14 @@ public:
         
         int s = 0;
         int e = nums.size() - 1;
-        const int left_min = nums[0];
         
         while (s < e) {
             int mid = s + (e-s)/2;
+            
             if (nums[mid] > nums[mid + 1])
                 return nums[mid + 1];
-            else if (nums[mid] >= left_min)
+            
+            if (nums[mid] >= nums[0])
                 s = mid + 1;
             else
                 e = mid;
