148543df580b4c689e141b465e30b9c4e93463e7
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-05-16 21:56:56 +0900

    Time: 81 ms (66.31%), Space: 19.2 MB (93.95%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,29 +1,34 @@
 class Solution {
-    vector<pair<int, int>> dir= {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    const vector<pair<int, int>> dir= {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int N = grid.size();
+        int N = grid.size() - 1;
+        if (grid[0][0] || grid[N][N])
+            return -1;
+        
         
         queue<pair<int, int>> q;
         q.push({0, 0});
+        grid[0][0] = 1;
         
-        for (int d = 1; !q.empty(); ++d) 
-            for (int i = 0, len = q.size(); i < len; i++) {
-                int x = q.front().first;
-                int y = q.front().second;
+        while (!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
 
-                q.pop();
+            q.pop();
 
-                if (x < 0 || y < 0 || x >= N || y >= N || grid[x][y])
+            if (x == N && y == N)
+                return grid[N][N];
+
+            for (auto& p : dir) {
+                int nx = x + p.first;
+                int ny = y + p.second;
+                if (nx < 0 || nx > N || ny < 0 || ny > N || grid[nx][ny])
                     continue;
-                if (x == N - 1 && y == N - 1)
-                    return d;
-                grid[x][y] = 1;
-
-
-                for (auto& p : dir)
-                    q.push({x + p.first, y + p.second});
+                q.push({nx, ny});
+                grid[nx][ny] = grid[x][y] + 1;
             }
+        }
         
         return -1;
     }
