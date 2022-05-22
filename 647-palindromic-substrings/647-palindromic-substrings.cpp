16ce31c2ff375a11eeed1e4818fada37125f3ea8
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-05-22 21:03:21 +0900

    Time: 6 ms (76.20%), Space: 6.3 MB (82.67%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) 
+            ans += getCount(s, i, i) + getCount(s, i, i+1);
+        return ans;
+    }
+    
+    int getCount(string& s, int left, int right) {
+        int cnt = 0;
+        while (left >= 0 && right < s.length() && s[left--] == s[right++]) 
+            ++cnt;
+        return cnt;
+    }
+};
