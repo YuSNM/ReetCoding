d9fc4113afa52b71187916f8c464f3e3a7f4f480
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-05-28 14:09:42 +0900

    Time: 17 ms (89.00%), Space: 18 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,14 +2,6 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         
-        unordered_map<int, int> m;
-        
-        for (int i : nums)
-            ++m[i];
-        
-        int ans = -1;
-        while (m[++ans]);
-        
-        return ans;
+        return (nums.size() + 1) * nums.size() / 2 - accumulate(nums.begin(), nums.end(), 0);
     }
 };
