580a524c767de1b289898c7536f30c244636c4de
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-06-10 00:18:14 +0900

    Time: 24 ms (26.00%), Space: 15.7 MB (44.00%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -6,12 +6,10 @@ public:
         int j = numbers.size() - 1;
         
         while(1) {
-            if (numbers[i] + numbers[j] < target) {
+            if (numbers[i] + numbers[j] < target)
                 i = lower_bound(numbers.begin() + i + 1, numbers. begin() + j, target - numbers[j]) - numbers.begin();
-            }
-            else if (numbers[i] + numbers[j] > target) {
+            else if (numbers[i] + numbers[j] > target) 
                 j = upper_bound(numbers.begin() + i + 1, numbers. begin() + j, target - numbers[i]) - numbers.begin() - 1;
-            }
             else
                 return {i + 1, j + 1};
         }
