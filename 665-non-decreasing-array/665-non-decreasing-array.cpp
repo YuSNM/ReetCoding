f3f2c5463b913d8b4e3ee3ce986708c1b238259e
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-06-25 10:20:08 +0900

    Time: 26 ms (94.77%), Space: 27.1 MB (53.59%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
-        int idx = -1;
         for (int i = 0, j = nums.size() - 1, cnt = 0; i < j; i++)
-            if (nums[i] > nums[i+1]) {
-                if (cnt++)
-                    return 0;
-                idx = i;
-            }
-        
-        if (idx > 0 && idx + 2 < nums.size() && nums[idx-1] > nums[idx+1] && nums[idx] > nums[idx + 2])
-            return 0;
+            if (nums[i] > nums[i+1] && (cnt++ || i > 0 && i + 1 < j && nums[i-1] > nums[i+1] && nums[i] > nums[i + 2]))
+                return 0;
         return 1;
     }
 };
