7f6f96ec3af4cf98f5598d486550f1ba1a09355b
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-07-03 15:02:39 +0900

    Time: 5 ms (48.51%), Space: 7.1 MB (75.45%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        if (nums.size() == 1)
+            return 1;
+        int ans = 2, i = 1;
+        while (nums[i] == nums[0])
+            if (++i >= nums.size())
+                return 1;
+        
+        int sign = nums[i] > nums[0];
+        
+        while (++i < nums.size())
+            if (sign && nums[i-1] > nums[i]) {
+                ans++;
+                sign = 0;
+            }  else if (!sign && nums[i-1] < nums[i]) {
+                ans++;
+                sign = 1;
+            }
+        
+        return ans;
+    }
+};
