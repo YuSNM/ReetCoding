c16215d56c757df162c03918ec670df6802a6e6d
YuSNM <101610563+YuSNM@users.noreply.github.com>
2022-07-06 12:45:45 +0900

    Time: 2 ms (64.32%), Space: 5.9 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,12 @@
 class Solution {
+    
+    int box[31] = {0, 1, 1, };
 public:
     int fib(int n) {
-        if (n < 3)
-            return (n+1) / 2;
-        return fib(n-1) + fib(n-2);
+        if (n == 0)
+            return 0;
+        if (box[n])
+            return box[n];
+        return box[n] = fib(n-1) + fib(n-2);
     }
 };
